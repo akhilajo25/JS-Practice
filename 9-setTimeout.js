11f8f12ec0b21720acc15e5a3c671ca460c7ae5b
akhila <akhila.josi@geogo.in>
2021-09-28 18:50:00 +0530

    setTimeout, setInterval and clearInterval

---
+++ 9-setTimeout.js
@@ -0,0 +1,15 @@
+console.log("----------------SET TIMEOUT----------------")
+/*setTimeOut expects a callback fuction and time  */
+
+setTimeout(() =>{
+    console.log("There you go!, you've waited enough!")
+},3000)
+
+
+console.log("----------------SET INTERVAL----------------")
+
+// setInterval(() =>{
+//     console.log(Math.random())
+// },3000)
+
+clearInterval(id)
